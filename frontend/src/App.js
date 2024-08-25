b8a78bc6a74ec143fc28b8dfa7c1c4a99ddb65ea
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-08-25 12:18:43 +0300

    implement product categories.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import kid_banner from './Components/Assets/banner_kids.png';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
 import Cart from './Pages/Cart';
@@ -15,9 +18,18 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<Shop />} />
-					<Route path="/men" element={<ShopCategory category="men" />} />
-					<Route path="/women" element={<ShopCategory category="women" />} />
-					<Route path="/kids" element={<ShopCategory category="women" />} />
+					<Route
+						path="/men"
+						element={<ShopCategory banner={men_banner} category="men" />}
+					/>
+					<Route
+						path="/women"
+						element={<ShopCategory banner={women_banner} category="women" />}
+					/>
+					<Route
+						path="/kids"
+						element={<ShopCategory banner={kid_banner} category="kid" />}
+					/>
 					<Route path="*" element={<Product />} />
 					<Route path="/productId" element={<Product />} />
 					<Route path="/cart" element={<Cart />} />
--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = () => {
 		<div className="footer">
 			<div className="footer-logo">
 				<img src={footer_logo} alt="" />
+				<p>QuickBuy</p>
 			</div>
 			<ul className="footer-links">
 				<li>Company</li>
